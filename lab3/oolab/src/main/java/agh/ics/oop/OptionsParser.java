2fa43edbfeefad68ba4df579902acc6bc1b6413a
Julija <juleczek2002@gmail.com>
2022-10-20 17:20:57 +0200

    Parser commit

--- lab3/oolab/src/main/java/agh/ics/oop/OptionsParser.java
+++ lab3/oolab/src/main/java/agh/ics/oop/OptionsParser.java
@@ -1,10 +1,12 @@
 package agh.ics.oop;
 
+import java.util.Arrays;
+
 public class OptionsParser {
 
     public MoveDirection [] parse (String[] directions){
-        MoveDirection[] ans = new MoveDirection[0];    //tworzę tablicę, którą ma mi zwrócić metoda
-        int i = 0;
+        MoveDirection[] ans = new MoveDirection[directions.length];    //tworzę tablicę, którą ma mi zwrócić metoda
+        int i = 0; //aby wiedzieć, jaka jest faktyczna długość tablicy, którą mam zwrócić -> tylko kierunki wpisuję, gdy dane są złe, jest pusta tablica
 
         for (String direction : directions){
             if (direction.equals("f") || direction.equals("forward") || direction.equals("b") || direction.equals("backward") ||
@@ -37,6 +39,7 @@ public class OptionsParser {
 
             }
         }
+        ans = Arrays.copyOfRange(ans, 0, i); //ans to tylko tablica od 0 do momentu, gdzie są kierunki
         return ans;
     }
 
